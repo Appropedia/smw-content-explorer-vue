f2b6dd3456a6014ec1f30580b23f4347c95218a6
Joksan Alvarado <joksan.alvarado@gmail.com>
2026-08-21 17:33:49 -0600

    Updated NPM packages to their latest versions.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import ui from '@nuxt/ui/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
